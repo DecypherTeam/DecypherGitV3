1871bfe13fd630b4a77a951f62c8baaeb9fdffed
typicalbel <decyphergit@gmail.com>
2021-10-26 16:13:06 +0800

    Update for object pickup

--- Pengaga Ati V3/Assets/Scripts/ObjectPickUp.cs
+++ Pengaga Ati V3/Assets/Scripts/ObjectPickUp.cs
@@ -7,6 +7,7 @@ public class ObjectPickUp : MonoBehaviour
 {
     public GameObject player;
     public Transform pickUpDest;
+    public Rigidbody pickupitem;
 
     private SphereCollider sc = new SphereCollider();
     private Animator anim = new Animator();
@@ -16,7 +17,7 @@ public class ObjectPickUp : MonoBehaviour
     {
         sc = gameObject.GetComponent<SphereCollider>();
         anim = player.GetComponent<Animator>();
-        sc.radius = 1.5f;
+        sc.radius = 2.5f;
         isPickUp = false;
     }
 
@@ -24,17 +25,23 @@ public class ObjectPickUp : MonoBehaviour
     {
         if (TCKInput.GetAction("pickBtn", EActionEvent.Press))
         {
-            anim.SetBool("isPickup", true);
 
             if (isPickUp)
             {
+                anim.SetBool("isPickup", true);
                 transform.position = pickUpDest.position;
+                pickupitem.useGravity = false;
+                pickupitem.transform.parent = pickUpDest.transform;
+                pickupitem.constraints = RigidbodyConstraints.FreezeAll;
             }
         }
 
         if (TCKInput.GetAction("pickBtn", EActionEvent.Up))
         {
+            pickupitem.constraints = RigidbodyConstraints.None;
             anim.SetBool("isPickup", false);
+            pickupitem.useGravity = true;
+            pickupitem.transform.parent = null;
         }
     }
 
